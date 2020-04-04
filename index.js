892afb9170c6d67d5c1f6b53e7c248809b39ca3f
放心家 <sunjian.shanghai@foxmail.com>
2020-04-04 13:17:44 +0800

    初始化项目

---
+++ index.js
@@ -0,0 +1,13 @@
+import * as BaseUtils from "./lib/base";
+import * as BrowserUtils from "./lib/browser";
+import * as CheckUtils from "./lib/check";
+import * as DateUtils from "./lib/date";
+import * as StorageUtils from "./lib/storage";
+
+export {
+  BaseUtils,
+  BrowserUtils,
+  CheckUtils,
+  DateUtils,
+  StorageUtils
+}
